9cc33facd416b57f2396de28655b08bfa22dcb38
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-03-11 18:30:43 +0800

    init

---
+++ src/components/home/index.js
@@ -0,0 +1,14 @@
+import { h, Component } from '../../preact/src/preact.js';
+
+import style from './style';
+
+export default class Home extends Component {
+	render() {
+		return (
+			<div class={style.home}>
+				<h1>Home</h1>
+				<p>This is the Home component.</p>
+			</div>
+		);
+	}
+}
